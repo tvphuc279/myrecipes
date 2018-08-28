9c32d836ef04c2f6a1a24c171856cd5098f4311f
Phuc Truong <tvphuc279@gmail.com>
2018-08-28 10:22:53 +0000

    Add edit & delete recipe & tdd

--- test/integration/recipes_test.rb
+++ test/integration/recipes_test.rb
@@ -24,6 +24,8 @@ class RecipesTest < ActionDispatch::IntegrationTest
     assert_template 'recipes/show'
     assert_match @recipe.name, response.body
     assert_match @recipe.description, response.body
+    assert_select 'a[href=?]', edit_recipe_path(@recipe), text: "Edit this recipe"
+    assert_select 'a[href=?]', recipe_path(@recipe), text: "Delete this recipe"
   end
   
   test "should get recipe new" do 
@@ -48,5 +50,40 @@ class RecipesTest < ActionDispatch::IntegrationTest
     assert_template 'recipes/new'
     assert_select 'h2.panel-title'
     assert_select 'div.panel-body'
+  end
+  
+  test "reject invalid recipe update" do
+    get edit_recipe_path(@recipe)
+    assert_template 'recipes/edit'
+    patch recipe_path(@recipe), params: { recipe: { name: " ", 
+  
+                                                 description: "some description" } } 
+    assert_template 'recipes/edit'
+    assert_select 'h2.panel-title'
+    assert_select 'div.panel-body'
+  end
+  
+  test "successfully edit a recipe" do
+    get edit_recipe_path(@recipe)
+    assert_template 'recipes/edit'
+    updated_name = "updated recipe name"
+    updated_description = "updated recipe description"
+    patch recipe_path(@recipe), params: { recipe: { name: updated_name, description: updated_description } }
+    assert_redirected_to @recipe
+    assert_not flash.empty?
+    @recipe.reload
+    assert_match updated_name, @recipe.name
+    assert_match updated_description, @recipe.description
+  end
+  
+  test "successfully delete a recipe" do
+    get recipe_path(@recipe)
+    assert_template 'recipes/show'
+    assert_select 'a[href=?]', recipe_path(@recipe), text: "Delete this recipe"
+    assert_difference 'Recipe.count', -1 do
+      delete recipe_path(@recipe)
+    end
+    assert_redirected_to recipes_path
+    assert_not flash.empty?
   end
 end
